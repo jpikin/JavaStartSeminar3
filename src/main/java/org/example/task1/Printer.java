7c163f438084968a3303b3a19fd295d06fd69ad7
j.pikin <pikin@bk.ru>
2023-07-28 17:10:18 +0500

    MegreSort added

--- src/main/java/org/example/task1/Printer.java
+++ src/main/java/org/example/task1/Printer.java
@@ -1,27 +1,50 @@
 package org.example.task1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class MergeSort {
-    public static int[] mergeSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            int min = array[i];
-            int minId = i;
-            for (int j = i+1; j < array.length; j++) {
-                if (array[j] < min) {
-                    min = array[j];
-                    minId = j;
-                }
-            }
-            // замена
-            int temp = array[i];
-            array[i] = min;
-            array[minId] = temp;
+    public static int[] mergeSort(int[] arr, int n) {
+        if (n < 2) {
+            return arr;
         }
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
 
-    return array;
+        for (int i = 0; i < mid; i++) {
+            l[i] = arr[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = arr[i];
+        }
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+
+        merge(arr, l, r, mid, n - mid);
+
+        return arr;
     }
-}
+
+    public static void merge(
+        int[] a, int[] l, int[] r, int left, int right) {
+
+            int i = 0, j = 0, k = 0;
+            while (i < left && j < right) {
+                if (l[i] <= r[j]) {
+                    a[k++] = l[i++];
+                } else {
+                a[k++] = r[j++];
+            }
+        }
+            while (i < left) {
+                a[k++] = l[i++];
+            }
+            while (j < right) {
+                a[k++] = r[j++];
+            }
+        }
+    }
 
 public class Printer {
     public static void main(String[] args) {
@@ -34,8 +57,13 @@ public class Printer {
             a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
         }
 
-        MergeSort answer = new MergeSort();
-        String itresume_res = Arrays.toString(answer.mergeSort(a));
-        System.out.println(itresume_res);
+
+        int[] result = MergeSort.mergeSort(a, a.length);
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < result.length; i++) {
+            list.add(result[i]);
+
+        }
+        System.out.println(list);
     }
 }
